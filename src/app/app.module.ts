72fca285686d3b07cf9f79fe0eaed759137c4624
gopi <linkin.go@gmail.com>
2017-08-09 20:21:33 +0200

    implement router for employee and role component and added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './component/employee/employee.component';
@@ -10,6 +11,23 @@ import { RoleComponent } from './component/role/role.component';
 import {RoleDataServices} from './component/role/role.services';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'emp',
+    component: EmployeeComponent
+  },
+  {
+    path: 'role',
+    component: RoleComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 
 @NgModule({
@@ -18,9 +36,11 @@ import { LoginComponent } from './component/login/login.component';
     EmployeeComponent,
     RoleComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
