46a2d3d402aae317340f3829cce1445e58a2e780
gopi <linkin.go@gmail.com>
2017-08-12 10:16:12 +0200

    added new Employee form component

---
+++ src/app/component/employee-forms/employee-forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-forms',
+  templateUrl: './employee-forms.component.html',
+  styleUrls: ['./employee-forms.component.css']
+})
+export class EmployeeFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
